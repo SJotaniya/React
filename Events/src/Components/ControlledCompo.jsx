adffa055e1a5b3ada56a0333b010cd89b9274b24
SJotaniya <saurabhjotaniya6531@outlook.com>
2024-09-30 15:51:04 +0530

    project6

--- Events/src/Components/ControlledCompo.jsx
+++ Events/src/Components/ControlledCompo.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function ControlledCompo() {
+  const [name,setName] = useState("")
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1>Controlled Component</h1>
+      <input type="text" placeholder='Enter Name' onChange={(e) => handleChange(e)}/>
+    </>
   )
 }
--- Local_Storage/src/Day1/CRUD.jsx
+++ Local_Storage/src/Day1/CRUD.jsx
@@ -1,31 +1,52 @@
 import React, { useEffect, useState } from 'react'
 
 export default function CRUD() {
-  const [name,setName] = useState("")
-  const [subject,setSubject] = useState("")
-  const [city,setCity] = useState("")
+  const [name,setName] = useState('')
+  const [subject,setSubject] = useState('')
+  const [city,setCity] = useState('')
 
-  const [record,setRecord] = useState("")
+  const [record,setRecord] = useState([]);
+  const [editIndex,setEditIndex] = useState(null);	
 
-  useEffect(()=>{
-    let data = JSON.parse(localStorage.getItem("stuData")) || []
+  useEffect (() => {
+    let data = JSON.parse(localStorage.getItem("student"))
     setRecord(data)
-  })
-  
-  const handelData = () => {
-    let userData = {id: Date.now(),name,subject,city}
-    record.push(userData)
-    localStorage.setItem("stuData", JSON.stringify(record))
+  },[])
+
+  const handleAdd = () => {
+    let data1 = {id: Date.now(),name,subject,city}
+    let newRecord = [...record, data1];
+    localStorage.setItem("student",JSON.stringify(newRecord))
+
+    setName("")
+    setSubject("")
+    setCity("")
+  }
+
+  const handleEdit = (id) => {
+    let getData = record.find((item) => item.id == id)
+    
+    setName(getData.name)
+    setSubject(getData.subject)
+    setCity(getData.city)
+    setEditIndex(id)
+  }
+
+  const handleDelete = (id) => {
+    let deleteData = record.filter((item) => item.id !== id )
+    setRecord(deleteData)
+    localStorage.setItem("student",JSON.stringify(deleteData))
   }
 
   return (
     <>
-      <h1>CRUD Operations</h1>
-      <input type="text" placeholder='Enter Your Name' onChange={(e) => setName(e.target.value)}/>
-      <input type="text" placeholder='Enter Subject' onChange={(e) => setSubject(e.target.value)} />
-      <input type="text" placeholder='Enter Your City' onChange={(e) => setCity(e.target.value)} />
+      <h1>Local Storage ke sath Chhed-Chhaad</h1>
 
-      <button onClick={handelData}>Set Data</button>
+      <input type="text" placeholder='Enter Your Name' onChange={(e)=>setName(e.target.value)}/>
+      <input type="text" placeholder='Enter Subject' onChange={(e)=>setSubject(e.target.value)}/>
+      <input type="text" placeholder='Enter City' onChange={(e)=>setCity(e.target.value)}/>
+
+      <button onClick={handleAdd}>{editIndex? "update" : "add"}</button>
 
       <table border={1}>
         <thead>
@@ -34,21 +55,23 @@ export default function CRUD() {
             <th>Name</th>
             <th>Subject</th>
             <th>City</th>
+            <th colSpan={2}>Action</th>
           </tr>
         </thead>
         <tbody>
           {
-            record 
-            ? record.map((e,i) => { 
+            record ? 
+            record.map((e,i) => {
               return <tr key={i}>
                 <td>{e.id}</td>
                 <td>{e.name}</td>
                 <td>{e.subject}</td>
                 <td>{e.city}</td>
-                </tr>
-              })
-            : " "
-          }
+                <td><button onClick={(e) => handleEdit(e.id)}>Edit</button></td>
+                <td><button onClick={(e) => handleDelete(e.id)}>Delete</button></td>
+              </tr>
+            }) : ''
+          } 
         </tbody>
       </table>
     </>
