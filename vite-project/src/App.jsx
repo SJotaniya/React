7aedb6c1fddda3fce2e7c633d047d95f7a68b053
SJotaniya <saurabhjotaniya6531@outlook.com>
2024-10-16 19:41:24 +0530

    Work

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Task from './Tasks/Task'
+import './App.css'
+import Project from './Projects/Project'
+
+function App() {
+  return(
+    <>
+      <Task/>
+      {/* <Project/> */}
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/Tasks/Task.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReduxCounter from '../Tasks/Redux/redux/ReduxCounter'
+
+export default function Task() {
+  return (
+    <>
+      <ReduxCounter/>
+    </>
+  )
+}
